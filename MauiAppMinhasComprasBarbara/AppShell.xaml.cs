8b95045954ee787ec962472e7a2ecc0b25f80ad5
Barbara Chabunas <babichabunas@gmail.com>
2025-03-23 17:35:17 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasComprasBarbara/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppMinhasComprasBarbara
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
